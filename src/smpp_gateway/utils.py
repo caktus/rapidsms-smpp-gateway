3f0621899d1faf60a6b6efd81c106c431b1d7c30
Tobias McNulty <tobias@caktusgroup.com>
2021-12-03 01:51:24 +0000

    docstring cleanup

--- src/smpp_gateway/utils.py
+++ src/smpp_gateway/utils.py
@@ -3,9 +3,8 @@ import itertools
 
 def grouper(iterable, n):
     """
-    Collect data into fixed-length chunks or blocks. Handy for grouping lists of
-    objects into batches suitable for bulk_create(), as an alternative to
-    BatchOperations(), below.
+    Collect data into fixed-length chunks or blocks. Handy for grouping iterables of
+    objects into batches suitable for bulk_create().
     """
     # In case iterable is not an iterator (but is, e.g., a list), islice()
     # will return the first group indefinitely, so ensure iterable is an
